694b8d28f31a1968596692339d5aff4c72c4a063
sergio vieira <sergiosvieira@gmail.com>
2017-05-13 10:40:59 -0300

    Initial Commit

---
+++ lauren/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lauren
+//
+//  Created by sergio vieira on 13/05/17.
+//  Copyright © 2017 sergio vieira. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
